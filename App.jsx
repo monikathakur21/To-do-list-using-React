b4209f55a48f9ed0677dc0359d617cfd40be4ff0
monikathakur21 <114375443+monikathakur21@users.noreply.github.com>
2024-05-04 08:36:45 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function App() {
+const [inputText,setInputText] = React.useState("");
+const [items,setItems]=React.useState([]);
+function handleChange(event){
+  const newValue=event.target.value;
+  setInputText(newValue);
+}
+function handleClick() {
+  setItems(prevItems=>{
+    return [...prevItems,inputText]
+  })
+}
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText}/>
+        <button onClick={handleClick}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map(todoItem=> <li>{todoItem}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
